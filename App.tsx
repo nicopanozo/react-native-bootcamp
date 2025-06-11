f62b6331ac1ff8f74e973fcf3f1a9b9c493aefc3
Nicolas Panozo <nicopanozoc@gmail.com>
2025-06-11 09:31:23 -0400

    Improved styles

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Home from './src/screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home/>
+      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
@@ -13,9 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingHorizontal: 16,
-    justifyContent: 'center',
   },
 });
--- src/screens/Slider.tsx
+++ src/screens/Slider.tsx
@@ -1,18 +1,12 @@
-// screens/Slider.tsx
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Slider = () => {
-  const [count, setCount] = useState(1);
+  const [count] = useState(1);
 
   return (
     <View style={styles.container}>
       <Text style={styles.counter}>{count}</Text>
-
-      <View style={styles.buttons}>
-        <Button title="WishList" color="green" onPress={() => alert('Go to WishList')} />
-        <Button title="Details" color="green" onPress={() => alert('Go to Details')} />
-      </View>
     </View>
   );
 };
@@ -21,17 +15,10 @@ export default Slider;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center'
   },
   counter: {
     fontSize: 40,
-    marginBottom: 20
   },
-  buttons: {
-    flexDirection: 'row',
-    gap: 10, // solo funciona en expo SDK 49+ o RN 0.73+, si no, usa marginRight en los botones
-    marginTop: 20
-  }
 });
