f9d426ee7baf87f954799271a56538154b0f0f7b
Nicolas Panozo <nicopanozoc@gmail.com>
2025-06-25 11:26:11 -0400

    Extract Movie interface to /types/index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  backdrop_path: string | null;
+  poster_path: string | null;
+  vote_average: number;
+  release_date?: string;
+  original_language?: string;
+}
